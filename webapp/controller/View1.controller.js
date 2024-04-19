9aa396467628d17f4554fbf04981cb9c205b7003
Sreeram Sreedhar <sreeramsap123@gmail.com>
2024-04-19 09:06:10 +0000

    added global model

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,21 +9,32 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("project1.controller.View1", {
-            onInit: function () {
-                const oPerson = {
-                    employee: {
-                        EmployeeName: "John Doe",
-                        EmployeeAge: 40,
-                        ContractStarted: new Date(),
-                        Email: "john.doe@gmail.com"
-                    }
-                }
-                const oJsonModel = new JSONModel(oPerson);
-                this.getView().setModel(oJsonModel);
+            onInit: function () { }
+
+            //     const oPerson = {
+            //         employee: {
+            //             EmployeeName: "John Doe",
+            //             EmployeeAge: 40,
+            //             ContractStarted: new Date(),
+            //             Email: "john.doe@gmail.com"
+            //         }
+            //     }
+            //     const oPerson2 = {
+            //         employee: {
+            //             EmployeeName: " Doe",
+            //             EmployeeAge: 20,
+            //             ContractStarted: new Date(),
+            //             Email: "john.doe@gmail.com"
+            //         }
+            //     }
+            //     const oJsonModel = new JSONModel(oPerson);
+            //     const oJsonModel2 = new JSONModel(oPerson2);
+            //     this.getView().setModel(oJsonModel);
+            //     this.getView().setModel(oJsonModel2,"sree");
 
 
 
-            }
+            // }
 
         });
     });
